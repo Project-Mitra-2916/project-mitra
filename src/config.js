4e5837e43c49d11be16e46d4ea37895a8c85bbe4
Vedanshi <techvedanshi16@email.com>
2025-07-29 21:09:37 +0530

    Added CSS and config files by Vedanshi

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// src/config.js
+
+export const adminEmails = ["projectmitra2916@gmail.com"];
+
+export const siteConfig = {
+  name: "Project Mitra",
+  tagline: "Explore. Build. Share.",
+  themeToggle: true,
+};
